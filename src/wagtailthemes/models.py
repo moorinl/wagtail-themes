acd795713a27913dfedb224297b48abe2d9713bf
Nando Davis <nandodavis@gmail.com>
2019-07-10 16:08:37 +0200

    Add Wagtail 2.5 compatibility

--- src/wagtailthemes/models.py
+++ src/wagtailthemes/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext as _
 from wagtail.contrib.settings.models import BaseSetting, register_setting
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel
 
 __ALL__ = ['ThemeSettings']
 
