42d38e17773875b1ea954826889130ea64dea8d4
Martijn Jacobs <maerteijn@gmail.com>
2021-07-16 10:13:24 +0200

    Explicit define the id field to AutoField to prevent unwanted migrations
    When DEFAULT_AUTO_FIELD in your project is not set to AutoField an unwanted migration will be added

--- src/wagtailthemes/models.py
+++ src/wagtailthemes/models.py
@@ -5,19 +5,20 @@ from django.utils.translation import gettext as _
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 
-__ALL__ = ['ThemeSettings']
+__ALL__ = ["ThemeSettings"]
 
 
 @register_setting
 class ThemeSettings(BaseSetting):
-    THEMES = getattr(settings, 'WAGTAIL_THEMES', None)
+    THEMES = getattr(settings, "WAGTAIL_THEMES", None)
 
+    id = models.AutoField(
+        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+    )
     theme = models.CharField(blank=True, max_length=255, null=True)
 
-    panels = [
-        FieldPanel('theme', widget=forms.Select(choices=THEMES))
-    ]
+    panels = [FieldPanel("theme", widget=forms.Select(choices=THEMES))]
 
     class Meta:
-        verbose_name = _('themes')
-        verbose_name_plural = _('themes')
+        verbose_name = _("themes")
+        verbose_name_plural = _("themes")
