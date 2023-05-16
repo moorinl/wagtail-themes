59f5ebcea63ad16a2926e889c3c44761aa4b492b
Rob Moorman <rob@moori.nl>
2023-05-16 10:43:11 +0200

    chore: version bump 0.5.0 (#30)

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 
 install_requires = [
-    'django>=3.1',
-    'wagtail>=2.12'
+    'django>=4.2',
+    'wagtail>=5.0'
 ]
 
 test_require = [
@@ -17,11 +17,11 @@ test_require = [
 
 setup(
     name='wagtail-themes',
-    version='0.4.1',
+    version='0.5.0',
     description='Site specific theme loader for Django Wagtail.',
     author='Rob Moorman',
     author_email='rob@moori.nl',
-    url='https://github.com/moorinteractive/wagtail-themes',
+    url='https://github.com/moorinl/wagtail-themes',
     license='MIT',
     install_requires=install_requires,
     extras_require={
